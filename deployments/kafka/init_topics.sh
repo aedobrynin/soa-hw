5bbc1f87ab67e736d8fe6cd86c76728297f52bc0
Anton <43320720+aedobrynin@users.noreply.github.com>
2024-04-28 17:19:58 +0300

    Posts views in statistics

--- deployments/kafka/init_topics.sh
+++ deployments/kafka/init_topics.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-kafka-topics.sh --create --if-not-exists --topic $KAFKA_LIKES_TOPIC_NAME --bootstrap-server kafka:9092
+kafka-topics.sh --create --if-not-exists --topic $KAFKA_POSTS_LIKES_TOPIC_NAME --bootstrap-server kafka:9092
+kafka-topics.sh --create --if-not-exists --topic $KAFKA_POSTS_VIEWS_TOPIC_NAME --bootstrap-server kafka:9092
