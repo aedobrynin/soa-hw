488ca53f068d22bd7cfebcac052a35ad8fbc6379
Anton <hashlib@yandex.ru>
2024-06-09 19:51:24 +0300

    Added 1 int. test

--- integration_tests/test_posts.py
+++ integration_tests/test_posts.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 
 # TODO: more tests here
@@ -27,3 +29,28 @@ def test_post_retrieve(make_user, auth_user, make_request, core_addr):
     assert post['id'] == post_id
     assert post['content'] == post_content
     assert 'author_id' in post   # TODO: test author_id is valid
+
+
+def test_like_post(make_user, auth_user, make_request, core_addr):
+    user = auth_user(make_user())
+    post_content = 'post_content'
+    resp = make_request(
+        'POST', core_addr, '/v1/posts', data={'content': post_content}, cookies=user.auth_cookies
+    )
+    assert resp.status_code == 200
+    post_id = resp.json()['post_id']
+
+    resp = make_request(
+        'POST', core_addr, f'/v1/posts/{post_id}/mark_liked', cookies=user.auth_cookies
+    )
+    assert resp.status_code == 200
+
+    time.sleep(3)   # Wait for Kafka + ClickHouse processing
+
+    resp = make_request('GET', core_addr, f'/v1/posts/{post_id}/stats', cookies=user.auth_cookies)
+    assert resp.status_code == 200
+    assert resp.json() == {
+        'post_id': post_id,
+        'views_count': 0,
+        'likes_count': 1,
+    }
